7066aa0a83bb6b159313ccec3a148e0021681d33
belaletech <belalahmadswnbhr@gmail.com>
2023-05-06 03:02:53 +0530

    update

---
+++ 01Assignment/tempCodeRunnerFile.java
@@ -0,0 +1,8 @@
+ // if(a==0 )
+    // {
+    //   System.out.println(" HCF Of Two number is "+ b);
+    // }
+    // if(b==0)
+    // {
+    //   System.out.println("Hcf of two number is " +a);
+    // }

d4926ca477996a42e8cac97b7177609673fa098b
belaletech <belalahmadswnbhr@gmail.com>
2023-05-10 01:23:14 +0530

    update

---
+++ 01Assignment/sumOfNumber.java
@@ -0,0 +1,14 @@
+//5.Keep taking numbers as inputs till the user enters ‘x’, after that print sum of all.
+import java.util.*;
+public class sumOfNumber {
+  public static void main(String [] args)
+  {
+    Scanner sc=new Scanner(System.in);
+    int a=sc.nextInt();
+    // int b=sc.nextInt();
+    // int sum=a+b;
+    // System.out.println("Sum of two number :"+sum);
+    // System.out.println(sum);
+  }
+  
+}
---
+++ methodstring/Test.java
@@ -0,0 +1,13 @@
+package methodstring;
+
+//import javax.lang.model.element.Element;
+
+ public class Test {
+  public static void main(String [] args)
+  {
+    String s1="Belal";
+    String s2="java";
+    System.out.println(String.join(";",s2, s1));
+  }
+  
+}
